9fd3573a618c3c76874e6b85c480cd21ff2aa0eb
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-11-20 11:26:33 +0300

    addVenue updated in pool facade

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const PoolFacade  = require('./pool-facade.js');
 
 const poolFacade = PoolFacade.create();
-poolFacade.addVenue('h','B','C','D',0, 1,2);
+
+poolFacade.addVenue({
+    venueId: 'dsadsdasddfs',
+    name: 'B',
+    category: 'C',
+    address: 'D',
+    likes: 2,
+    lon: 3,
+    lat: 5
+});
--- pool-facade.js
+++ pool-facade.js
@@ -11,12 +11,17 @@ class PoolFacade  {
         this.pool = pool;
     }
 
-    addVenue(venueId, name, category, address, likes, lon, lat) {
+    addVenue(venue) {
         const query = `
         insert into test0(venue_id, name, category, address, likes, geom) VALUES ($1,$2,$3,$4,$5,st_setsrid(st_makepoint($6,$7), 4326));
         `
 
-        this.pool.query(query, [venueId, name, category,address, likes, lon, lat]);
+        this.pool.query(query, Object.values(venue), (err, result) => {
+            if(err) {
+                return console.error('Error in addVenue method with parameter', venue, 'executing  query: ', err.detail);
+            }
+            console.log("no any error");
+        });
     }
 }
 
